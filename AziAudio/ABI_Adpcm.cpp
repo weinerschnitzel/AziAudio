41fe389b42f51e9ee35e15969d183fdcabc53b53
unknown <rj150@hotmail.com>
2015-05-13 19:30:14 -0400

    Move `for' loop limit to its own variable for a neater header.

--- AziAudio/ABI_Adpcm.cpp
+++ AziAudio/ABI_Adpcm.cpp
@@ -416,31 +416,42 @@ void ADPCM3() { // Verified to be 100% Accurate...
 
 void LOADADPCM() { // Loads an ADPCM table - Works 100% Now 03-13-01
 	u32 v0;
+	size_t i, limit;
+
 	v0 = (t9 & 0xffffff);// + SEGMENTS[(t9>>24)&0xf];
 	//	if (v0 > (1024*1024*8))
 	//		v0 = (t9 & 0xffffff);
 	//	memcpy (dmem+0x4c0, rdram+v0, k0&0xffff); // Could prolly get away with not putting this in dmem
 	//	assert ((k0&0xffff) <= 0x80);
 	u16 *table = (u16 *)(rdram + v0);
-	for (u32 x = 0; x < ((k0 & 0xffff) >> 0x4); x++)
-		swap_elements(&adpcmtable[8*x], &table[8*x]);
+
+	limit = (k0 & 0x0000FFFF) >> 4;
+	for (i = 0; i < limit; i++)
+		swap_elements(&adpcmtable[8*i], &table[8*i]);
 }
 
 void LOADADPCM2() { // Loads an ADPCM table - Works 100% Now 03-13-01
 	u32 v0;
+	size_t i, limit;
+
 	v0 = (t9 & 0xffffff);// + SEGMENTS[(t9>>24)&0xf];
 	u16 *table = (u16 *)(rdram + v0); // Zelda2 Specific...
 
-	for (u32 x = 0; x < ((k0 & 0xffff) >> 0x4); x++)
-		swap_elements(&adpcmtable[8*x], &table[8*x]);
+	limit = (k0 & 0x0000FFFF) >> 4;
+	for (i = 0; i < limit; i++)
+		swap_elements(&adpcmtable[8*i], &table[8*i]);
 }
 
 void LOADADPCM3() { // Loads an ADPCM table - Works 100% Now 03-13-01
 	u32 v0;
+	size_t i, limit;
+
 	v0 = (t9 & 0xffffff);
 	//memcpy (dmem+0x3f0, rdram+v0, k0&0xffff);
 	//assert ((k0&0xffff) <= 0x80);
 	u16 *table = (u16 *)(rdram + v0);
-	for (u32 x = 0; x < ((k0 & 0xffff) >> 0x4); x++)
-		swap_elements(&adpcmtable[8*x], &table[8*x]);
+
+	limit = (k0 & 0x0000FFFF) >> 4;
+	for (i = 0; i < limit; i++)
+		swap_elements(&adpcmtable[8*i], &table[8*i]);
 }
