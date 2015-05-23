a3c3cda72088f05b0af8624a512355bc707e53df
unknown <rj150@hotmail.com>
2015-05-23 18:50:23 -0400

    moved rdot() accumulation to its own vectorizable loop

--- AziAudio/ABI_Filters.cpp
+++ AziAudio/ABI_Filters.cpp
@@ -192,8 +192,10 @@ void POLEF()
 
 		for (i = 0; i < 8; ++i) {
 			accumulators[i]  = frame[i] * Gain;
-			accumulators[i] += h1[i]*l1 + h2_before[i]*l2 + rdot(i, h2, frame);
+			accumulators[i] += h1[i]*l1 + h2_before[i]*l2;
 		}
+		for (i = 0; i < 8; ++i)
+			accumulators[i] += rdot(i, &h2[0], &frame[0]);
 		for (i = 0; i < 8; ++i)
 			dst[i ^ 1] = pack_signed(accumulators[i] >> 14);
 
